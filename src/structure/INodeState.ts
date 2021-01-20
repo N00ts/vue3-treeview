5c8de9b5f9ade416f44c4bb1acf29a775748b9a9
Tristan <tristanfontaine3000@gmail.com>
2021-01-20 22:47:08 +0100

    added missing files

---
+++ src/structure/INodeState.ts
@@ -0,0 +1,12 @@
+export interface INodeState {
+    opened?: boolean;
+    disabled?: boolean;
+    editing?: boolean;
+    focusable?: boolean;
+    selectable?: boolean;
+    selected?: boolean;
+    draggable?: boolean;
+    dropable?: boolean;
+    checked?: boolean;
+    indeterminate?: boolean;
+}
