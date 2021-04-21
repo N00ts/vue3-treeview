7d0232c7a6ca2f368f433920a18a84d449581353
N00ts <tristanfontaine3000@gmail.com>
2021-04-21 19:41:38 +0200

    updated vuepress config file

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,5 +1,5 @@
 import { DefaultThemeOptions, defineUserConfig } from "vuepress"
 
 export default defineUserConfig<DefaultThemeOptions>({
-    base: "vue3-treeview"
+    base: "/vue3-treeview/"
 });
