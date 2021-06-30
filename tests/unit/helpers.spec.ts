33290eb0a30e9fc7e242d9e4b4adbbe7c2b5419d
N00ts <tristanfontaine3000@gmail.com>
2021-06-30 18:12:02 +0200

    updated to cover 100% branches

--- tests/unit/helpers.spec.ts
+++ tests/unit/helpers.spec.ts
@@ -1,8 +1,18 @@
 import { INode } from "../../src/structure/INode";
 import { ensureState } from '../../src/misc/helpers';
 
-test("test", () => {
+test("Expect to initialize state", () => {
     const n : INode = {};
     ensureState(n);
     expect(n.state).toBeDefined();
 });
+
+test("Expect not to initalize state", () => {
+    const n: INode = {
+        state: {
+            checked: true
+        }
+    }
+    ensureState(n);
+    expect(n.state).toMatchObject(n.state);
+})
