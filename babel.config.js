4f7b87b09389ca207d2f9c413b8ea9d6bba49f61
N00ts <tristanfontaine3000@gmail.com>
2021-07-07 17:02:32 +0200

    updated babel config for jest debug

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    [
+      '@babel/preset-env', {targets: {node: 'current'}}],
+      '@babel/preset-typescript',
   ],
   plugins: ["dynamic-import-node"]
 }
